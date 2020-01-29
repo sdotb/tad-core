98f081508458553edc0f9e397f48d813fe019560
Unknown <s.bertoli@ictec.it>
2020-01-30 00:38:23 +0100

    Bugfix Exception in MoodWorker

--- src/TADCore/TADWorkerMood.php
+++ src/TADCore/TADWorkerMood.php
@@ -13,13 +13,13 @@ class TADWorkerMood extends abTADWorker
     {
         $requested_class = 'SdotB\\Mood\\'.$this->type;
         if (!class_exists($requested_class)) {
-            throw new Exception("worng argument or not implemented", 1);
+            throw new \Exception("worng argument or not implemented", 1);
         }
 
         $mood_instance = new $requested_class($this->options);
 
         if(!(in_array("permittedActions",get_class_methods($mood_instance)) && in_array($this->action,$mood_instance->permittedActions()))) {
-            throw new Exception("wrong action or not permitted", 2);
+            throw new \Exception("wrong action or not permitted", 2);
         }
 
         $parsed_data = [];
