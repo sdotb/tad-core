718f60e992381688b6410705db67cbcbd269698b
Unknown <s.bertoli@ictec.it>
2020-04-19 02:56:59 +0200

    TADWorkerMood Bugfix
    try catch implementation in parseData method

--- src/TADCore/TADWorkerMood.php
+++ src/TADCore/TADWorkerMood.php
@@ -11,23 +11,27 @@ class TADWorkerMood extends abTADWorker
 
     public function parseData(): array
     {
-        $requested_class = 'SdotB\\Mood\\'.$this->type;
-        if (!class_exists($requested_class)) {
-            $this->tad->setTWrong('wrong argument or not implemented');
+        try {
+            $requested_class = 'SdotB\\Mood\\'.$this->type;
+            if (!class_exists($requested_class)) {
+                $this->tad->setTWrong('wrong argument or not implemented');
+                throw new \Exception();
+            }
+
+            $mood_instance = new $requested_class($this->options);
+            if(!(in_array("permittedActions",get_class_methods($mood_instance)) && in_array($this->action,$mood_instance->permittedActions()))) {
+                $this->tad->setAWrong("wrong action or not permitted");
+                throw new \Exception();
+            }
+
+            $parsed_data = [];
+            $parsed_data = $mood_instance->{$this->action}($this->data);
+            unset($mood_instance);
+            $this->tad->parsedData($parsed_data);
+
+        } catch (\Throwable $th) {
+            $parsed_data = [];
         }
-
-        $mood_instance = new $requested_class($this->options);
-
-        if(!(in_array("permittedActions",get_class_methods($mood_instance)) && in_array($this->action,$mood_instance->permittedActions()))) {
-            $this->tad->setAWrong("wrong action or not permitted");
-        }
-
-        $parsed_data = [];
-        $parsed_data = $mood_instance->{$this->action}($this->data);
-        unset($mood_instance);
-
-        $this->tad->parsedData($parsed_data);
-
         return $parsed_data;
     }
 }
